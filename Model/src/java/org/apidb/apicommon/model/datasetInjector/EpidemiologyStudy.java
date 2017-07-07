dfb8cf8745809d3dde0b76583203c64ddd52d1e3
John Brestelli <jbrestell@gmail.com>
2017-07-07 18:31:46 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiModel/trunk@83055 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/java/org/apidb/apicommon/model/datasetInjector/EpidemiologyStudy.java
@@ -0,0 +1,39 @@
+package org.apidb.apicommon.model.datasetInjector;
+
+
+import org.apidb.apicommon.datasetPresenter.DatasetInjector;
+
+public class EpidemiologyStudy extends DatasetInjector {
+
+
+  @Override
+  public void injectTemplates() {}
+
+  @Override
+  public void addModelReferences() {
+      addWdkReference("ClinicalVisitRecordClasses.ClinicalVisitRecordClass", "question", "ClinicalVisitQuestions.ClinicalVisitsByAllData"); 
+      addWdkReference("ClinicalVisitRecordClasses.ClinicalVisitRecordClass", "question", "ClinicalVisitQuestions.ClinicalVisitBySourceID"); 
+      addWdkReference("ClinicalVisitRecordClasses.ClinicalVisitRecordClass", "question", "ClinicalVisitQuestions.ClinicalVisitsByRelativeVisits"); 
+      addWdkReference("DwellingRecordClasses.DwellingRecordClass", "question", "DwellingQuestions.DwellingsBySourceID"); 
+      addWdkReference("ParticipantRecordClasses.ParticipantRecordClass", "question", "ParticipantQuestions.ParticipantsByAllData"); 
+      addWdkReference("ParticipantRecordClasses.ParticipantRecordClass", "question", "ParticipantQuestions.ParticipantsByRelativeVisits"); 
+      addWdkReference("ParticipantRecordClasses.ParticipantRecordClass", "question", "ParticipantQuestions.ParticipantsByClinicalVisit"); 
+      addWdkReference("ParticipantRecordClasses.ParticipantRecordClass", "question", "ParticipantQuestions.ParticipantsBySourceID"); 
+
+      addWdkReference("ParticipantRecordClasses.ParticipantRecordClass", "table", "Visits");
+      addWdkReference("ParticipantRecordClasses.ParticipantRecordClass", "table", "Samples");
+      addWdkReference("ParticipantRecordClasses.ParticipantRecordClass", "table", "Characteristics");
+      addWdkReference("ClinicalVisitRecordClasses.ClinicalVisitRecordClass", "table", "Samples");
+      addWdkReference("ClinicalVisitRecordClasses.ClinicalVisitRecordClass", "table", "Characteristics");
+      addWdkReference("DwellingRecordClasses.DwellingRecordClass", "table", "Characteristics");
+  }
+
+
+  @Override
+  public String[][] getPropertiesDeclaration() {
+    String[][] propertiesDeclaration = {};
+    return propertiesDeclaration;
+  }
+
+
+}
