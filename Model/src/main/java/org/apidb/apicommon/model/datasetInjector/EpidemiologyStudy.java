072c69e26f85158375b2748400aa86210dadfcdc
John Brestelli <jbrestell@gmail.com>
2017-10-13 20:55:21 +0000

    injectors for records (questions still broken)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiModel/trunk@85041 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/main/java/org/apidb/apicommon/model/datasetInjector/EpidemiologyStudy.java
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/EpidemiologyStudy.java
@@ -1,13 +1,97 @@
 package org.apidb.apicommon.model.datasetInjector;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.apidb.apicommon.datasetPresenter.DatasetInjector;
 
 public class EpidemiologyStudy extends DatasetInjector {
 
+    private String addQuotes(String s) {
+        List<String> split = Arrays.asList(s.split("\\s*,\\s*"));
+        List<String> quoted = new ArrayList<>();
+
+        for (String str : split) {
+            quoted.add("'" + str + "'");
+        }
+        String result = String.join(",", quoted);
+
+        return result;
+    }
 
   @Override
-  public void injectTemplates() {}
+  public void injectTemplates() {
+
+      String projectName = getPropValue("projectName");
+      String datasetName = getDatasetName();
+
+      String includeProjects = getPropValueAsBoolean("isPublic") ? "ClinEpiDB," + projectName : projectName;
+
+      boolean hasHouseholds = getPropValueAsBoolean("hasHouseholdRecord");
+      boolean hasParticipants = getPropValueAsBoolean("hasParticipantRecord");
+      boolean hasObservations = getPropValueAsBoolean("hasObservationRecord");
+
+      if(hasHouseholds) {
+
+          String householdSourceIdsForHouseholdMemberTable = getPropValue("householdSourceIdsForHouseholdMemberTable");
+          setPropValue("householdSourceIdsForHouseholdMemberTableQuote", addQuotes(householdSourceIdsForHouseholdMemberTable));
+
+          injectTemplate("householdRecord");
+          injectTemplate("householdRecordAttributeQueries");
+          injectTemplate("householdRecordTableQueries");
+
+          //TODO inject metadata filter question
+
+      }
+      if(hasParticipants) {
+
+          String householdSourceIdsIncludedInParticipantAttributes = getPropValue("householdSourceIdsIncludedInParticipantAttributes");
+          String participantSourceIdsExcludedFromParticipantAttributes = getPropValue("participantSourceIdsExcludedFromParticipantAttributes");
+          String observationSourceIdsForParticipantsObservationsTable  = getPropValue("observationSourceIdsForParticipantsObservationsTable");
+
+          setPropValue("householdSourceIdsIncludedInParticipantAttributesQuote", addQuotes(householdSourceIdsIncludedInParticipantAttributes));
+          setPropValue("participantSourceIdsExcludedFromParticipantAttributesQuote", addQuotes(participantSourceIdsExcludedFromParticipantAttributes));
+          setPropValue("observationSourceIdsForParticipantsObservationsTableQuote", addQuotes(observationSourceIdsForParticipantsObservationsTable));
+
+          injectTemplate("participantRecord");
+          injectTemplate("participantRecordAttributeQueries");
+          injectTemplate("participantRecordTableQueries");
+
+          // TODO:  model ref for visit table and sample table if has those things
+          //TODO inject metadata filter question
+      }
+      if(hasObservations) {
+
+          String householdSourceIdsIncludedInObservationAttributes = getPropValue("householdSourceIdsIncludedInObservationAttributes");
+          String participantSourceIdsIncludedInObservationAttributes = getPropValue("participantSourceIdsIncludedInObservationAttributes");
+          String hhAndPIncludedInObservationAttributes = householdSourceIdsIncludedInObservationAttributes + "," + participantSourceIdsIncludedInObservationAttributes;
+
+          setPropValue("participantAndHouseholdSourceIdsIncludedInObservationAttributesQuote", addQuotes(hhAndPIncludedInObservationAttributes));
+
+          injectTemplate("observationRecord");
+          injectTemplate("observationRecordAttributeQueries");
+          injectTemplate("observationRecordTableQueries");
+
+          // model ref for visit table and sample table if has those things
+          // TODO inject metadata filter question
+      }
+
+      if(hasHouseholds && hasParticipants) {
+          // inject transform question
+          // inject answer param
+      }
+      if(hasHouseholds && hasObservations) {
+          // inject transform question
+          // inject answer param
+      }
+      if(hasParticipants && hasObservations) {
+          // inject transform question
+          // inject answer param
+      }
+
+
+  }
 
   @Override
   public void addModelReferences() {
@@ -34,8 +118,33 @@ public class EpidemiologyStudy extends DatasetInjector {
 
   @Override
   public String[][] getPropertiesDeclaration() {
-    String[][] propertiesDeclaration = {};
-    return propertiesDeclaration;
+
+      String [][] declaration = {
+                                 {"isPublic", ""},
+                                 {"hasHouseholdRecord", ""},
+                                 {"hasObservationRecord", ""},
+                                 {"hasParticipantRecord", ""},
+
+                                 {"householdAttributesList", ""},
+                                 {"householdSourceIdsForHouseholdMemberTable", ""},
+                                 {"householdRecordOverview", ""},
+
+                                 {"observationAttributesList", ""},
+                                 {"observationRecordOverview", ""},
+                                 {"householdSourceIdsIncludedInObservationAttributes", ""},
+                                 {"participantSourceIdsIncludedInObservationAttributes", ""},
+
+                                 {"participantAttributesList", ""},
+                                 {"participantRecordOverview", ""},
+                                 {"participantGraphAttributes", ""},
+                                 {"participantGraphAttributeNames", ""},
+                                 {"householdSourceIdsIncludedInParticipantAttributes", ""},
+                                 {"participantSourceIdsExcludedFromParticipantAttributes", ""},
+                                 {"observationSourceIdsForParticipantsObservationsTable", ""},
+                                 {"observationSourceIdsToOrderParticipantsObservationsTable", ""},
+      };
+
+    return declaration;
   }
 
 
