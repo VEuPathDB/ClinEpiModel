f0848649c0e552a32cf67db103acbbe3ca9e81d9
John Brestelli <jbrestell@gmail.com>
2017-12-12 20:30:32 +0000

    bysourceid and transform questions;  model refs from injectsion
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiModel/trunk@85938 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/main/java/org/apidb/apicommon/model/datasetInjector/EpidemiologyStudy.java
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/EpidemiologyStudy.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apidb.apicommon.datasetPresenter.ModelReference;
 import org.apidb.apicommon.datasetPresenter.DatasetInjector;
 
 public class EpidemiologyStudy extends DatasetInjector {
@@ -23,6 +24,8 @@ public class EpidemiologyStudy extends DatasetInjector {
   @Override
   public void injectTemplates() {
 
+      addModelReferences();
+
       String subProjectName = getPropValue("subProjectName");
       //      String datasetName = getDatasetName();
       String includeProjects = getPropValueAsBoolean("isPublic") ? "ClinEpiDB," + subProjectName : subProjectName;
@@ -36,22 +39,26 @@ public class EpidemiologyStudy extends DatasetInjector {
       boolean hasParticipants = getPropValueAsBoolean("hasParticipantRecord");
       boolean hasObservations = getPropValueAsBoolean("hasObservationRecord");
 
-      if(hasHouseholds) {
+      // TODO: how to handle optional tables??  probably just do these in a subclass?
 
+      if(hasHouseholds) {
           String householdSourceIdsForHouseholdMemberTable = getPropValue("householdSourceIdsForHouseholdMemberTable");
           setPropValue("householdSourceIdsForHouseholdMemberTableQuote", addQuotes(householdSourceIdsForHouseholdMemberTable));
 
           injectTemplate("householdRecord");
           injectTemplate("householdRecordAttributeQueries");
           injectTemplate("householdRecordTableQueries");
+          
+          // Household SourceId Question
+          injectTemplate("householdSourceIdQuestion");
+          injectTemplate("householdSourceIdQuery");
+          injectTemplate("householdSourceIdParam");
 
-          //TODO inject metadata filter question
-
+          injectTemplate("householdResultParam");
       }
 
 
       if(hasParticipants) {
-
           String householdSourceIdsIncludedInParticipantAttributes = getPropValue("householdSourceIdsIncludedInParticipantAttributes");
           String participantSourceIdsExcludedFromParticipantAttributes = getPropValue("participantSourceIdsExcludedFromParticipantAttributes");
           String observationSourceIdsForParticipantsObservationsTable  = getPropValue("observationSourceIdsForParticipantsObservationsTable");
@@ -64,11 +71,16 @@ public class EpidemiologyStudy extends DatasetInjector {
           injectTemplate("participantRecordAttributeQueries");
           injectTemplate("participantRecordTableQueries");
 
-          // TODO:  model ref for visit table and sample table if has those things
-          //TODO inject metadata filter question
+          // Participant SourceId Question
+          injectTemplate("participantSourceIdQuestion");
+          injectTemplate("participantSourceIdQuery");
+          injectTemplate("participantSourceIdParam");
+
+          injectTemplate("participantResultParam");
       }
 
       if(hasObservations) {
+
 
           String householdSourceIdsIncludedInObservationAttributes = getPropValue("householdSourceIdsIncludedInObservationAttributes");
           String participantSourceIdsIncludedInObservationAttributes = getPropValue("participantSourceIdsIncludedInObservationAttributes");
@@ -82,47 +94,112 @@ public class EpidemiologyStudy extends DatasetInjector {
           injectTemplate("observationRecordAttributeQueries");
           injectTemplate("observationRecordTableQueries");
 
-          // model ref for visit table and sample table if has those things
-          // TODO inject metadata filter question
+          // Observation: SourceId Question
+          injectTemplate("observationSourceIdQuestion");
+          injectTemplate("observationSourceIdQuery");
+          injectTemplate("observationSourceIdParam");
+
+          injectTemplate("observationResultParam");
       }
 
-
+      // Household->Participant and Participant->Household
       if(hasHouseholds && hasParticipants) {
-          // inject transform question
-          // inject answer param
-      }
-      if(hasHouseholds && hasObservations) {
-          // inject transform question
-          // inject answer param
-      }
-      if(hasParticipants && hasObservations) {
-          // inject transform question
-          // inject answer param
+          injectTemplate("householdsByParticipantQuestion");
+          injectTemplate("householdsByParticipantQuery");
+
+          injectTemplate("participantsByHouseholdsQuestion");
+          injectTemplate("participantsByHouseholdsQuery");
       }
 
+      // Participant->Observations and Observations->Participants
+      if(hasParticipants && hasObservations) {
+          injectTemplate("participantsByObservationsQuestion");
+          injectTemplate("participantsByObservationsQuery");
+
+          injectTemplate("observationsByParticipantsQuestion");
+          injectTemplate("observationsByParticipantsQuery");
+      }
+
+
+      // Add categories for all of the model references
+      for (ModelReference modelReference : getModelReferences()) {
+          String categoryIri = modelReference.getCategoryIri();
+          String targetName = modelReference.getTargetName();
+          String recordClassName = modelReference.getRecordClassName();
+          String targetType = modelReference.getTargetType().equals("question") ? "search" : modelReference.getTargetType();
+          String[] scopes = modelReference.getScopes();
+
+          setPropValue("targetType", targetType);
+          setPropValue("targetName", targetName);
+          setPropValue("recordClassName", recordClassName);
+          setPropValue("categoryIri", categoryIri);          
+          setPropValue("uniqueName", recordClassName + "." + targetName);
+
+          setPropValue("scopes", String.join("\t", scopes));
+
+          if(scopes != null && categoryIri != null) {
+              injectTemplate("clinEpiCategory");
+          }
+      }
 
   }
 
   @Override
   public void addModelReferences() {
+
+      String categoryIri = "http://edamontology.org/topic_3305";
+
+      String presenterId = getPropValue("presenterId");
+
+      String observationRecordClass = presenterId + "ObservationRecordClasses." + presenterId + "ObservationRecordClass";
+      String participantRecordClass = presenterId + "ParticipantRecordClasses." + presenterId + "ParticipantRecordClass";
+      String householdRecordClass = presenterId + "HouseholdRecordClasses." + presenterId + "HouseholdRecordClass";
+
+      addWdkReference(householdRecordClass, "table", "Characteristics", new String[]{"record"}, categoryIri);
+      addWdkReference(householdRecordClass, "table", "HouseholdMembers", new String[]{"record"}, categoryIri);
+
+      addWdkReference(participantRecordClass, "table", "Characteristics", new String[]{"record"}, categoryIri);
+      addWdkReference(participantRecordClass, "table", "Observations", new String[]{"record"}, categoryIri);
+      // TODO Samples table of participant record page
+
+      addWdkReference(observationRecordClass, "table", "Characteristics", new String[]{"record"}, categoryIri);
+      // TODO Samples table of observation record page
+
+      // Add the Text Attributes
+      String participantGraphAttributeNames = getPropValue("participantGraphAttributeNames");
+      String[] participantGraphsArray = participantGraphAttributeNames.split(",");
+      for (int i = 0; i < participantGraphsArray.length; i++) {
+          addWdkReference(participantRecordClass, "attribute", participantGraphsArray[i], new String[]{"record","results"}, categoryIri);
+      }
+
+      // By Source Id Questions
+      addWdkReference(participantRecordClass, "question", "ParticipantQuestions." + presenterId + "ParticipantsBySourceID", new String[]{"menu","webservice"}, categoryIri); 
+      addWdkReference(householdRecordClass, "question", "HouseholdQuestions." + presenterId + "HouseholdsBySourceID", new String[]{"menu","webservice"}, categoryIri); 
+      addWdkReference(observationRecordClass, "question", "ObservationQuestions." + presenterId + "ObservationssBySourceID", new String[]{"menu","webservice"}, categoryIri); 
+
+      addWdkReference(observationRecordClass, "question", "ObservationQuestions." + presenterId + "ObservationsByParticipants", new String[]{"webservice"}, categoryIri); 
+      addWdkReference(participantRecordClass, "question", "ParticipantQuestions." + presenterId + "ParticipantsByHouseholds", new String[]{"webservice"}, categoryIri); 
+      addWdkReference(participantRecordClass, "question", "ParticipantQuestions." + presenterId + "ParticipantsByObservations", new String[]{"webservice"}, categoryIri); 
+      addWdkReference(householdRecordClass, "question", "HouseholdQuestions." + presenterId + "HouseholdsByParticipants", new String[]{"webservice"}, categoryIri); 
+
+
+      /**
       addWdkReference("ClinicalVisitRecordClasses.ClinicalVisitRecordClass", "question", "ClinicalVisitQuestions.ClinicalVisitsByAllData"); 
-      addWdkReference("ClinicalVisitRecordClasses.ClinicalVisitRecordClass", "question", "ClinicalVisitQuestions.ClinicalVisitBySourceID"); 
       addWdkReference("ClinicalVisitRecordClasses.ClinicalVisitRecordClass", "question", "ClinicalVisitQuestions.ClinicalVisitsByRelativeVisits"); 
-      addWdkReference("DwellingRecordClasses.DwellingRecordClass", "question", "DwellingQuestions.DwellingsBySourceID"); 
+
       addWdkReference("ParticipantRecordClasses.ParticipantRecordClass", "question", "ParticipantQuestions.ParticipantsByAllData"); 
       addWdkReference("ParticipantRecordClasses.ParticipantRecordClass", "question", "ParticipantQuestions.ParticipantsByRelativeVisits"); 
-      addWdkReference("ParticipantRecordClasses.ParticipantRecordClass", "question", "ParticipantQuestions.ParticipantsByClinicalVisit"); 
-      addWdkReference("ParticipantRecordClasses.ParticipantRecordClass", "question", "ParticipantQuestions.ParticipantsBySourceID"); 
-      /* MAL-ED */
+
       addWdkReference("ClinicalVisitRecordClasses.ClinicalVisitRecordClass", "question", "ClinicalVisitQuestions.ClinicalVisitsByRelativeVisits_maled"); 
       addWdkReference("ParticipantRecordClasses.ParticipantRecordClass", "question", "ParticipantQuestions.ParticipantsByRelativeVisits_maled"); 
 
       addWdkReference("ParticipantRecordClasses.ParticipantRecordClass", "table", "Visits");
       addWdkReference("ParticipantRecordClasses.ParticipantRecordClass", "table", "Samples");
-      addWdkReference("ParticipantRecordClasses.ParticipantRecordClass", "table", "Characteristics");
+
       addWdkReference("ClinicalVisitRecordClasses.ClinicalVisitRecordClass", "table", "Samples");
-      addWdkReference("ClinicalVisitRecordClasses.ClinicalVisitRecordClass", "table", "Characteristics");
-      addWdkReference("DwellingRecordClasses.DwellingRecordClass", "table", "Characteristics");
+
+
+      **/
   }
 
 
