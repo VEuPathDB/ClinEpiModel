5d3c94a8736630b625a04ea934629dd935eec515
John Brestelli <jbrestell@gmail.com>
2017-12-14 15:27:54 +0000

    only add household transforms if hasHouseholds
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiModel/trunk@85991 735e2a04-f8fc-0310-8a1b-f2942603c481

--- Model/src/main/java/org/apidb/apicommon/model/datasetInjector/EpidemiologyStudyWithLightTraps.java
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/EpidemiologyStudyWithLightTraps.java
@@ -46,6 +46,8 @@ public class EpidemiologyStudyWithLightTraps extends EpidemiologyStudy {
 
         String presenterId = getPropValue("presenterId");
 
+        boolean hasHouseholds = getPropValueAsBoolean("hasHouseholdRecord");
+
         String householdRecordClass = makeRecordClassName(HOUSEHOLD_RECORD_CLASS_PREFIX);
         String participantRecordClass = makeRecordClassName(PARTICIPANT_RECORD_CLASS_PREFIX);
         String observationRecordClass = makeRecordClassName(OBSERVATION_RECORD_CLASS_PREFIX);
@@ -56,8 +58,11 @@ public class EpidemiologyStudyWithLightTraps extends EpidemiologyStudy {
 
         addWdkReference(lightTrapRecordClass, "question", "LighttrapQuestions." + presenterId + "CollectionsBySourceID", new String[]{"menu","webservice"}, CATEGORY_IRI); 
 
-        addWdkReference(lightTrapRecordClass, "question", "LighttrapQuestions." + presenterId + "CollectionsByHouseholdId", new String[]{"webservice"}, CATEGORY_IRI); 
-        addWdkReference(householdRecordClass, "question", "HouseholdQuestions." + presenterId + "HouseholdsByLightTrapId", new String[]{"webservice"}, CATEGORY_IRI); 
+        if(hasHouseholds) {
+            addWdkReference(lightTrapRecordClass, "question", "LighttrapQuestions." + presenterId + "CollectionsByHouseholdId", new String[]{"webservice"}, CATEGORY_IRI); 
+            addWdkReference(householdRecordClass, "question", "HouseholdQuestions." + presenterId + "HouseholdsByLightTrapId", new String[]{"webservice"}, CATEGORY_IRI); 
+        }
+
     }
 
 
