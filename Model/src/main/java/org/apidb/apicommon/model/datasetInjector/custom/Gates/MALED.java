8627ac82341e8af4fe2dde01a84235f9723f4d15
John Brestelli <jbrestell@gmail.com>
2018-03-02 18:41:08 +0000

    new java class for maled (phase1+)
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiModel/trunk@87536 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/custom/Gates/MALED.java
@@ -0,0 +1,67 @@
+package org.apidb.apicommon.model.datasetInjector.custom.Gates;
+
+import org.apidb.apicommon.model.datasetInjector.EpidemiologyStudy;
+import java.util.Map;
+import java.util.HashMap;
+
+public class MALED extends EpidemiologyStudy {
+
+    @Override
+    protected String participantGraphAttributes() {
+      return("");
+        //      return(getTemplateInstanceText("MALEDGraphAttributes"));
+    }
+
+    @Override
+    protected Map<String,String> participantGraphAttributeNames() {
+      return(new HashMap<String,String>());
+
+      /**
+      Map<String,String> map = new HashMap<String,String>();
+      map.put("allCompact", "demo_plot_allThree_compact");
+      map.put("weightHeight", "demo_plot_weightheight");
+      map.put("weightAge", "demo_plot_weightage");
+      map.put("heightAge", "demo_plot_heightage");
+      map.put("allThree", "demo_plot_allThree");
+      map.put("length", "demo_plot_xlength");
+      map.put("weight", "demo_plot_weight");
+
+      return(map);
+      **/
+    }
+
+    @Override
+    protected Map<String,String[]> participantGraphAttributeScopes() {
+      return(new HashMap<String,String[]>());
+
+      /**
+      Map<String,String[]> scopeMap = new HashMap<String,String[]>();
+      scopeMap.put("allCompact", new String[] {"results"});
+      scopeMap.put("weightHeight", new String[] {"record"});
+      scopeMap.put("weightAge", new String[] {"record"});
+      scopeMap.put("heightAge", new String[] {"record"});
+      scopeMap.put("allThree", new String[] {"record"});
+      scopeMap.put("length", new String[] {"record"});
+      scopeMap.put("weight", new String[] {"record"});
+
+      return(scopeMap);
+      **/
+    }
+
+    @Override
+    public void addModelReferences() {
+        super.addModelReferences();
+
+        String presenterId = getPropValue("presenterId");
+
+        String participantRecordClass = makeRecordClassName(PARTICIPANT_RECORD_CLASS_PREFIX);
+        String observationRecordClass = makeRecordClassName(OBSERVATION_RECORD_CLASS_PREFIX);
+
+        // TODO:  add wdk reference for questions
+        //        addWdkReference(participantRecordClass, "question", "ParticipantQuestions.ParticipantsByRelativeVisits_maled", new String[]{"menu", "webservice"}, CATEGORY_IRI);
+        //        addWdkReference(observationRecordClass, "question", "ClinicalVisitQuestions.ClinicalVisitsByRelativeVisits_maled", new String[]{"menu", "webservice"}, CATEGORY_IRI);
+
+    }
+
+
+}
