b4f676c10184f3d55bb2373b7b1a2e0cac4fc63e
Danielle Callan <dcallan@upenn.edu>
2019-05-09 18:21:34 +0000

    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/ClinEpiModel/trunk@98100 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/custom/ICEMR/Amazonia.java
@@ -0,0 +1,23 @@
+package org.apidb.apicommon.model.datasetInjector.custom.ICEMR;
+
+import org.apidb.apicommon.model.datasetInjector.BasicEpiStudy;
+import java.util.Map;
+import java.util.HashMap;
+
+public class Amazonia extends BasicEpiStudy {
+
+    @Override
+    protected String participantGraphAttributesTemplateName() {
+      return("AmazoniaICEMRGraphAttributes");
+    }
+
+    @Override
+    protected Map<String,String[]> participantGraphAttributesToScopes() {
+      Map<String,String[]> scopeMap = new HashMap<String,String[]>();
+      scopeMap.put("malaria_cat_compact", new String[] {"results"});
+      scopeMap.put("malaria_cat_svg", new String[] {"record"});
+      return(scopeMap);
+    }
+
+
+}
