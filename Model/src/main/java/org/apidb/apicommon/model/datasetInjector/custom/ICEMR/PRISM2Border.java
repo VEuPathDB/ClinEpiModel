38b6a5a0b62fdbfeaee0a0a59b4a23a797f6287f
Brian Brunk <brunkb@upenn.edu>
2021-03-16 16:55:11 -0400

    fixed date issue for prism border cohort

---
+++ Model/src/main/java/org/apidb/apicommon/model/datasetInjector/custom/ICEMR/PRISM2Border.java
@@ -0,0 +1,46 @@
+package org.apidb.apicommon.model.datasetInjector.custom.ICEMR;
+
+import org.apidb.apicommon.model.datasetInjector.BasicEpiStudyWithLightTraps;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PRISM2Border extends BasicEpiStudyWithLightTraps {
+
+    @Override
+    protected void setStudySpecificProperties(){
+        setPropValue("visitMinDate","2020-08-01");
+        setPropValue("visitMaxDate","2021-12-31");
+    }  
+
+    @Override
+    protected Map<String,String[]> participantQuestionTemplateNamesToScopes() {
+      Map<String,String[]> map = new HashMap<String,String[]>();
+      map.put("ParticipantsByMetadata_prism2", new String[] {"menu", "webservice"});
+
+      return(map);
+    }
+
+    @Override
+    protected Map<String,String[]> observationQuestionTemplateNamesToScopes() {
+      Map<String,String[]> map = new HashMap<String,String[]>();
+      map.put("ObservationsByMetadata_prism2", new String[] {"menu", "webservice"});
+
+      return(map);
+    }
+
+    @Override
+    protected String participantGraphAttributesTemplateName() {
+      return("PRISMCaseControlGraphAttributes");
+    }
+
+    @Override 
+    protected Map<String,String[]> participantGraphAttributesToScopes() {
+      Map<String,String[]> scopeMap = new HashMap<String,String[]>();
+      scopeMap.put("malaria_cat_compact", new String[] {"results"});
+      scopeMap.put("malaria_cat_svg", new String[] {"record"});
+
+      return(scopeMap);
+    }
+
+
+}
